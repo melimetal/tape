6e77311ea63a0cd558b86f0fb873cbf2eeb53ac3
melimetal <melimetal.contact@gmail.com>
2025-08-07 16:27:30 -0500

    function: add applyFlipped

--- FPME/src/CH5.purs
+++ FPME/src/CH5.purs
@@ -1,14 +1,8 @@
 module CH5 where
 
-import Prelude (Unit, show)
+import Prelude (Unit, show, discard)
 import Effect (Effect)
 import Effect.Console (log)
-
--- Defining operators with precedence!
-apply :: forall a b. (a -> b) -> a -> b
-apply f x = f x 
-
-infixr 0 apply as $
 
 -- NOTE: Constant function not value.
 const :: forall a b. a -> b -> a
@@ -18,7 +12,18 @@ const x _ = x
 flip :: forall a b c. (a -> b -> c) -> b -> a -> c
 flip f x y = f y x
 
+-- Defining operators with precedence!
+apply :: forall a b. (a -> b) -> a -> b
+apply f x = f x 
+
+infixr 0 apply as $
+
+applyFlipped :: forall a b. a -> (a -> b) -> b
+applyFlipped = flip apply
+
+infixl 1 applyFlipped as #
+
 test :: Effect Unit
 test = do
   log $ show $ flip const 1 2
-
+  flip const 1 2 # show # log
