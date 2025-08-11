af1e66f5c14a0e9558152d7c7151903f6d3fd816
melimetal <melimetal.contact@gmail.com>
2025-08-11 17:32:07 -0500

    FPME: add more Data.List related functions

--- FPME/src/CH5.purs
+++ FPME/src/CH5.purs
@@ -1,45 +1,68 @@
 module CH5 where
 
-import Prelude (Unit, show, discard)
+import Prelude (Unit, show, discard, (+))
 
 import Data.List (List(..), (:))
+import Data.Maybe (Maybe(..))
 import Effect (Effect)
 import Effect.Console (log)
 
 -- NOTE: Constant function not value.
-const :: forall a b. a -> b -> a
+const ∷ ∀ a b. a → b → a
 const x _ = x
 
 -- Silly but useful function
-flip :: forall a b c. (a -> b -> c) -> b -> a -> c
+flip ∷ ∀ a b c. (a → b → c) → b → a → c
 flip f x y = f y x
 
 -- Defining operators with precedence!
-apply :: forall a b. (a -> b) -> a -> b
-apply f x = f x 
+apply ∷ ∀ a b. (a → b) → a → b
+apply f x = f x
 
 infixr 0 apply as $
 
-applyFlipped :: forall a b. a -> (a -> b) -> b
+applyFlipped ∷ ∀ a b. a → (a → b) → b
 applyFlipped = flip apply
 
 infixl 1 applyFlipped as #
 
 -- Lists to study recursive functions
-singleton :: forall a. a -> List a
+singleton ∷ ∀ a. a → List a
 singleton x = x : Nil
 
 -- Check for empty values (alt. end of the list)
-null :: forall a. List a -> Boolean
+null ∷ ∀ a. List a → Boolean
 null Nil = true
-null _   = false
+null _ = false
 
 -- Does the inverse of cons (adds to the end of List)
-snoc :: forall a. List a -> a -> List a
-snoc Nil x        = singleton x
-snoc (x : xs) y   = x : snoc xs y 
+snoc ∷ ∀ a. List a → a → List a
+snoc Nil x = singleton x
+snoc (x : xs) y = x : snoc xs y
 
-test :: Effect Unit
+-- Self-explanatory
+length ∷ ∀ a. List a → Int
+length Nil = 0
+length (_ : xs) = length xs + 1
+
+-- Tail recursive version
+length' ∷ ∀ a. List a → Int
+length' l = go 0 l
+  where
+  go ∷ Int → List a → Int
+  go acc Nil = acc
+  go acc (_ : xs) = go (acc + 1) xs
+
+-- Maybe adds type safety even if it "takes" more "work"
+head ∷ ∀ a. List a → Maybe a
+head Nil = Nothing
+head (x : _) = Just x
+
+tail ∷ ∀ a. List a → Maybe (List a)
+tail Nil = Nothing
+tail (_ : xs) = Just xs
+
+test ∷ Effect Unit
 test = do
   log $ show $ flip const 1 2
   flip const 1 2 # show # log
@@ -47,3 +70,10 @@ test = do
   log $ show $ null Nil
   log $ show $ null ("abc" : Nil)
   log $ show $ snoc (1 : 2 : Nil) 3
+  log $ show $ length $ 1 : Nil
+  log $ show $ length $ 1 : 2 : 3 : Nil
+  log $ show $ length' $ 1 : 2 : 3 : Nil
+  log $ show (head Nil :: Maybe Unit)
+  log $ show $ head ("abc" : "123" : Nil)
+  log $ show (tail Nil :: (Maybe (List Unit)))
+  log $ show $ tail ("abc" : "123" : "sfdsf" : Nil)
